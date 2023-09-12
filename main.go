62d58fc65be3b2d5beafe6848c82e7bbb73b88a4
Daniel Koch <daniel.koch@publicare.de>
2023-09-12 12:39:17 +0200

    fixing mod stuff

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "bitbucket.org/publicaremarketing/pipeline-hero/cmd"
+import "github.com/PublicareDevelopers/pipeline-hero/cmd"
 
 func main() {
 	cmd.Execute()
