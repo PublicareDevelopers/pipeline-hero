e8c7342e5579cb6ed19731613f3093a897a49609
Daniel Koch <daniel.koch@publicare.de>
2025-01-14 12:32:37 +0100

    feat: some more audit output

--- sdk/cmds/composer.go
+++ sdk/cmds/composer.go
@@ -39,7 +39,7 @@ func GetComposerAudit() (string, error) {
 				return parseComposerAudit(string(out)), nil
 			}
 
-			fmt.Println(exitError.ExitCode(), "code caused composer audit fail", err)
+			fmt.Println(exitError.ExitCode(), "code caused composer audit fail", err, string(out))
 		}
 
 		return parseComposerAudit(string(out)), err
